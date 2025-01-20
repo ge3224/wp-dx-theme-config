edf17a5efb1095fc259ed6db8d3e778e5c703467
ge3224 <ge3224@gmail.com>
2025-01-20 17:02:49 -0600

    feat: add helper for template parts

--- src/template_parts/main.ts
+++ src/template_parts/main.ts
@@ -1,7 +1,23 @@
 import { TemplateParts } from "~/src/definitions/template_parts.ts";
 
-export default function newTemplateParts(): TemplateParts {
-  return [
+export function withTemplatePart(
+  templatePart: { name: string; title?: string; area?: string },
+): (tp: TemplateParts) => void {
+  return (tp: TemplateParts): void => {
+    const exists = tp.some((part) => part.name === templatePart.name);
+
+    if (!exists) {
+      tp.push(templatePart);
+    } else {
+      console.warn(
+        `Template part with name "${templatePart.name}" already exists.`,
+      );
+    }
+  };
+}
+
+/**
+ *
     {
       area: "header",
       name: "header",
@@ -12,5 +28,13 @@ export default function newTemplateParts(): TemplateParts {
       name: "footer",
       title: "Footer",
     },
-  ];
+ */
+export default function newTemplateParts(
+  ...mods: Array<(tp: TemplateParts) => void>
+): TemplateParts {
+  const tp: TemplateParts = [];
+
+  mods.forEach((mod) => mod(tp));
+
+  return tp;
 }
