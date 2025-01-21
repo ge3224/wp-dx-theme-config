edaf035333dadf85456709bc6f11889036c67b12
ge3224 <ge3224@gmail.com>
2025-01-20 18:35:20 -0600

    feat: add helper functions, custom templates

--- src/custom_templates/main.ts
+++ src/custom_templates/main.ts
@@ -1,7 +1,25 @@
-import { CustomTemplates } from "~/src/definitions/custom_templates.ts";
+import {
+  CustomTemplate,
+  CustomTemplates,
+} from "../definitions/custom_templates.ts";
 
-function newCustomTemplates(): CustomTemplates {
-  return [];
+export function withCustomTemplate(
+  c: CustomTemplate,
+): (ct: CustomTemplates) => void {
+  return (ct: CustomTemplates): void => {
+    const exists = ct.some((t) => t.name === c.name);
+    if (!exists) {
+      ct.push(c);
+    } else {
+      console.warn(`Custom Template with name "${c.name}" already exists`);
+    }
+  };
 }
 
-export default newCustomTemplates;
+export function newCustomTemplates(
+  ...mods: Array<(ct: CustomTemplates) => void>
+): CustomTemplates {
+  const ct: CustomTemplates = [];
+  mods.forEach((mod) => mod(ct));
+  return ct;
+}
--- src/definitions/custom_templates.ts
+++ src/definitions/custom_templates.ts
@@ -1,5 +1,7 @@
-export type CustomTemplates = Array<{
+export type CustomTemplate = {
   name: string;
   title: string;
   postTypes?: Array<string>;
-}>;
+};
+
+export type CustomTemplates = Array<CustomTemplate>;
